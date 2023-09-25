eb945376f2f25bcea526a2c8a847befd4b2d099a
nebojsa1803 <nikolicnebojsa1304@gmail.com>
2023-09-25 17:11:59 +0200

    routes separeted

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,5 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Home, Login, Register, Landing, Error } from './pages'
-import { action as registerAction } from './domain/auth/RegisterForm'
-import { action as loginAction } from './domain/auth/LoginForm'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Landing />,
-      },
-      {
-        path: 'login',
-        element: <Login />,
-        action: loginAction,
-      },
-      {
-        path: 'register',
-        element: <Register />,
-        action: registerAction,
-      },
-    ],
-  },
-])
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
 
 function App() {
   return <RouterProvider router={router} />
---
+++ src/routes.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from 'react-router-dom'
+import { Home, Login, Register, Landing, Error } from './pages'
+import { action as registerAction } from './domain/auth/RegisterForm'
+import { action as loginAction } from './domain/auth/LoginForm'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+        action: loginAction,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+        action: registerAction,
+      },
+    ],
+  },
+])
